49800fda466caaec9a37501f742cb4504ca62cbd
dcrystalj <dcrystalj@gmail.com>
2016-05-17 11:38:18 +0200

    fixed crontab for specific user because with sudo does not work

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo cp discover_session_bus.sh /opt/
 sudo cp bing-wallpaper.py /opt/
 sudo cp picture-fetcher.sh /etc/cron.daily/
 sudo cp changer.sh /etc/cron.hourly/
-#echo "@reboot DISPLAY=:0 /etc/crontab.hourly/changer.sh" >> /var/spool/cron/crontabs/$me
+echo "@reboot DISPLAY=:0 /etc/crontab.hourly/changer.sh" >> /var/spool/cron/crontabs/$me
+echo "0 * * * * /etc/crontab.hourly/changer.sh" >> /var/spool/cron/crontabs/$me
