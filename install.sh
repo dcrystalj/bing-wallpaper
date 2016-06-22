3ea2d09dc099175120b42981d1f78b8cdfb1c117
dcrystalj <dcrystalj@gmail.com>
2016-06-22 21:21:22 +0200

    fix cron path

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ sudo cp discover_session_bus.sh /opt/
 sudo cp bing-wallpaper.py /opt/
 sudo cp picture-fetcher.sh /etc/cron.daily/
 sudo cp changer.sh /etc/cron.hourly/
-echo "@reboot DISPLAY=:0 /etc/crontab.hourly/changer.sh" >> /var/spool/cron/crontabs/$me
-echo "0 * * * * /etc/crontab.hourly/changer.sh" >> /var/spool/cron/crontabs/$me
+sudo echo "@reboot DISPLAY=:0 /etc/cron.hourly/changer.sh" | sudo tee -a /var/spool/cron/crontabs/$me
+sudo echo "0 * * * * /etc/cron.hourly/changer.sh" | sudo tee -a /var/spool/cron/crontabs/$me
